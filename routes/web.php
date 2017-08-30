5d15e2cc2f3c238bc7bb5674bd98bad1fdea3f79
Peter Kumal <peter.kumal@olivemedia.co>
2017-08-30 17:12:01 +0545

    folder structure management and added login and register in backend

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,11 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/admin', function(){
+	return view('backend.admin.login');
+});
+
+Route::get('/admin/register', function(){
+	return view('backend.admin.register');
+});
